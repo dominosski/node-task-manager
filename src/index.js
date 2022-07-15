71ad00c554a2cff62aa881dfdcbd4a8a66261edd
dominosski <domix852@gmail.com>
2022-07-15 16:40:04 +0200

    Added jwt and test middleware

--- src/index.js
+++ src/index.js
@@ -8,6 +8,23 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+app.use((req, res, next) => {
+    if (req.method) {
+        res.status(503).send('Cannot use methods')
+    }
+    else {
+        next()
+    }
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
--- src/routers/user.js
+++ src/routers/user.js
@@ -27,10 +27,11 @@ router.get('/users/:id', async (req, res) => {
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
-
+    
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
     } catch (e) {
         res.status(400).send(e)
     }
@@ -68,8 +69,9 @@ router.patch('/users/:id', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
 
-        res.send(user)
+        res.send({ user, token })
     } catch (error) {
         res.status(400).send()
     }
