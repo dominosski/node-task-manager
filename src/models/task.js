cb34f36af8f21a035b28a1569551924eca9d1115
dominosski <domix852@gmail.com>
2022-07-18 17:37:24 +0200

    Added authentication to other endpoints and created relation between user and task

--- src/models/task.js
+++ src/models/task.js
@@ -8,8 +8,12 @@ const Task = mongoose.model('Task', {
     },
     completed: {
         type: Boolean,
-        required: false,
         default: false
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
     }
 })
 
